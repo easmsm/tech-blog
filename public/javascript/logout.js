ca16a94ed9304eaeafbe5e63c4f24b4e7630aa98
ekukwa <easmsm@gmail.com>
2022-10-11 16:50:01 -0400

    tried reinstalling bluebird, did not resolve error

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,4 +1,7 @@
+//added logout(EVENT) and event.preventDefault(); - didn't resolve error
+
 async function logout() {
+
   const response = await fetch('/api/users/logout', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' }
