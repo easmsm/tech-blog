67b5808d3cbe8a2c32c9c8a0da523526e278687e
ekukwa <easmsm@gmail.com>
2022-10-11 14:47:19 -0400

    added helper const to try to resolve error

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const routes = require('./controllers/')
+const helpers = require('./utils/helpers')
+
+// Handlebars
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,7 +29,6 @@ app.use(session(sess));
 
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
