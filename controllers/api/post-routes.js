825211ccd95e45d56a5565e5c3f4063012802e3e
ekukwa <easmsm@gmail.com>
2022-10-15 17:02:49 -0400

    updated controllers

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('/models');
-const sequelize = require('/config/connection');
-const withAuth = require('/utils/auth');
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-    console.log('======================');
     Post.findAll({
             attributes: ['id',
                 'title',
