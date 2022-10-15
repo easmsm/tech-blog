fabc0a17c7c187602a4766740226abc0eabec91a
ekukwa <easmsm@gmail.com>
2022-10-15 16:38:23 -0400

    corrected some script src links in handlebars, didn't go through /public before going to javascript. Main error not resolved yet

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+
 router.get('/', (req, res) => {
     Comment.findAll({})
         .then(dbCommentData => res.json(dbCommentData))
